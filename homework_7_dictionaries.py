8761f1629fde2b562b107dde57bd94883ddba2db
JakeFlippy <j-filippelli@hvcc.edu>
2025-10-28 11:18:12 -0400

    Update homework_7_dictionaries.py

--- homework_7_dictionaries.py
+++ homework_7_dictionaries.py
@@ -1,1 +1,6 @@
+#Similar to homework 4, write a program that takes a series of commands that allow the user to manipulate text in various ways. This time, you will be using a dictionary to keep track of words that should be replaced with other words. The commands should be as follows:
 
+#add: Adds a new substitution to the dictionary by asking for a word to be replaced and a word to replace it with.
+#describe: Prints out all active substitutions in the dictionary.
+#run: Ask the user for a sentence. Use your dictionary to make all of the necessary substitutions, then print out the resulting sentence.
+#exit: Exit the program.
